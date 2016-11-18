00da17946114f473fcdd793fe7c46c8f096ef930
RameshBista <meramesh111@gmail.com>
2016-11-18 17:43:24 +0545

    latest update

---
+++ app/src/main/java/com/example/user/educationhunt/fragment/Fee_College.java
@@ -0,0 +1,30 @@
+package com.example.user.educationhunt.fragment;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.user.educationhunt.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class Fee_College extends Fragment {
+
+
+    public Fee_College() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_fee__college, container, false);
+    }
+
+}
